114bd97d9d8026abf0ca08ff6e701a6afdd8daa6
Balt603 <balt603@gmail.com>
2024-08-28 22:19:12 +1000

    ui changes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,7 @@ import { Todo } from "./features/todo/Todo"
 
 const App = () => {
   return (
-    <div className="border-red w-3/5 border-2">
+    <div className="grid w-full place-items-center">
       <Todo />
     </div>
   )
--- ui/src/features/todo/Todo.tsx
+++ ui/src/features/todo/Todo.tsx
@@ -37,21 +37,39 @@ export const Todo = () => {
 
   if (data) {
     return (
-      <div>
+      <div className="grid w-11/12 place-items-center p-4 shadow-lg md:w-3/4 lg:w-1/2">
         {data.map(item => (
           <div
-            className="flex items-center justify-between rounded border border-gray-300 p-4"
+            className="flex w-full items-center justify-between border-b border-gray-300 py-1"
             key={item.id}
           >
             <label className="text-gray-700" htmlFor={item.id}>
-              {friendlyDate(item.dateCreated)} - {item.text}
+              {item.text}
             </label>
-            <input
-              className="form-checkbox h-5 w-5 text-blue-600"
-              type="checkbox"
-              id={item.id}
-              defaultChecked={item.isComplete}
-            ></input>
+            <div className="flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 512 512"
+                className="size-5 fill-green-700"
+              >
+                <g>
+                  <path d="M405.333,0H106.667C47.786,0.071,0.071,47.786,0,106.667v298.667C0.071,464.214,47.786,511.93,106.667,512h298.667   C464.214,511.93,511.93,464.214,512,405.333V106.667C511.93,47.786,464.214,0.071,405.333,0z M426.667,172.352L229.248,369.771   c-16.659,16.666-43.674,16.671-60.34,0.012c-0.004-0.004-0.008-0.008-0.012-0.012l-83.563-83.541   c-8.348-8.348-8.348-21.882,0-30.229s21.882-8.348,30.229,0l83.541,83.541l197.44-197.419c8.348-8.318,21.858-8.294,30.176,0.053   C435.038,150.524,435.014,164.034,426.667,172.352z" />
+                </g>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="Layer_1"
+                data-name="Layer 1"
+                viewBox="0 0 24 24"
+                className="size-5 fill-red-800"
+              >
+                <path d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Zm4.707,15.293-1.414,1.414L12,13.414,8.707,16.707,7.293,15.293,10.586,12,7.293,8.707,8.707,7.293,12,10.586l3.293-3.293,1.414,1.414L13.414,12Z" />
+              </svg>
+            </div>
           </div>
         ))}
       </div>
